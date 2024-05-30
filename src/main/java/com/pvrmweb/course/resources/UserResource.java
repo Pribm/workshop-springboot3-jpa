96f04f3fc0c3da652c192dd23b2a956dcf51e7b0
pribm <monteiro.paulovinicius@gmail.com>
2024-05-30 15:26:22 +1000

    Entidade User e seu Resource

---
+++ src/main/java/com/pvrmweb/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.pvrmweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pvrmweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user1 = new User(1, "Paulo", "paulo@test.com", "1234-5678", "password");
+		
+		 return ResponseEntity.ok().body(user1);
+	}
+}
